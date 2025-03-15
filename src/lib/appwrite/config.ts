4103433ff7abab200cad15ded8e9a46b5b315061
Rafael <rafaelhibiki@gmail.com>
2025-03-15 17:36:25 -0300

    feat: Move Appwrite configuration to environments file
    This commit refactors the Appwrite configuration by moving the database ID, bucket ID, bucket URL, and collection IDs to a new `src/lib/appwrite/enviroments.ts` file. This improves code organization and separation of concerns. The `src/lib/appwrite/config.ts` file was updated to remove the configuration variables and import the new environment variables. This change enhances maintainability and readability of the Appwrite configuration.

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,24 +1,6 @@
-import "server-only";
+"use server";
+
 import { Client, Databases, Account, Storage } from "node-appwrite";
-
-export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || "";
-export const BUCKET_ID = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID || "";
-export const APPWRITE_BUCKET_URL =
-  process.env.NEXT_PUBLIC_APPWRITE_BUCKET_URL || "";
-
-export const COLLECTIONS = {
-  users: "users",
-  destinations: process.env.NEXT_PUBLIC_APPWRITE_DESTINATIONS_ID!,
-  packages: process.env.NEXT_PUBLIC_APPWRITE_PACKAGES_ID!,
-  bookings: process.env.NEXT_PUBLIC_APPWRITE_BOOKINGS_ID!,
-  reviews: process.env.NEXT_PUBLIC_APPWRITE_REVIEWS_ID!,
-  promotions: process.env.NEXT_PUBLIC_APPWRITE_PROMOTIONS_ID!,
-  blog: process.env.NEXT_PUBLIC_APPWRITE_BLOG_ID!,
-  wishlist: process.env.NEXT_PUBLIC_APPWRITE_WISHLIST_ID!,
-  coupons: process.env.NEXT_PUBLIC_APPWRITE_COUPONS_ID!,
-  banners: process.env.NEXT_PUBLIC_APPWRITE_BANNERS_ID!,
-  email_templates: process.env.NEXT_PUBLIC_APPWRITE_EMAIL_TEMPLATES_ID!,
-};
 
 const createAdminClient = async () => {
   const client = new Client();
